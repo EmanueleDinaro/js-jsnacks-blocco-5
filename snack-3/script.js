9b711c78ba4c46565159595173a190ce5ab01ac1
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-17 12:38:59 +0100

    feat: add snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,8 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
+const newArr = numbers.map(elm => elm + 1);
+
+console.log(newArr);
+
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
