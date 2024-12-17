a64991b7d092eff618383f3db54eb3d09647a55f
Emanuele Dinaro <ema.dinaro@gmail.com>
2024-12-17 13:14:58 +0100

    feat: add snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,9 @@ const students = [
   },
 ];
 
+const student = students.find(elm => elm.id === 2);
+console.log(student); 
+
+
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
